7da63b40152132af4f71eae4ff95cf1f9903b7b5
PernilleLange <pernillel@gmail.comgit config --global --lisy #git config --global user.email pernillel@gmail.com>
2020-09-08 18:53:38 +0200

    Added vertical spacing

--- knitting_calculator.py
+++ knitting_calculator.py
@@ -49,28 +49,34 @@ def shoe_size():
             width = float(length * 1.05)
             return (length, width)
 
-def gauge():
-    """Returns a tuple containing the stitch gauge and row gauge pr. cm based on input from the user"""
+def stitch_gauge():
+    '''returns stitch gauge'''
     while True:
         try:
             stitch_gauge = float(input('Measuring over 10 cm / 4", how many stitches do you have? ')) / 10
+            return stitch_gauge
         except:
             print('Please write a number for your answer.')
             continue
+
+def row_gauge():
+    '''returns row gauge'''
+    while True:
         try:
             row_gauge = float(input('Measuring over 10 cm / 4", how many rows do you have? ')) / 10
-            return (stitch_gauge, row_gauge)
+            return row_gauge
         except:
             print('Please write a number for your answer.')
             continue
 
 class Spacing:
-    """Uses inital number and goal number to return a dictionary with values to distribute the difference evenly."""
+    """Uses inital number and goal number to return instructions to distribute sts evenly"""
     def __init__(self, in_num, st_dif):
         self.in_num = in_num
         self.st_dif = st_dif
+        self.st_quotient = self.in_num / self.st_dif
 
-    def odd_even_divisor(self):
+    def odd_even(self):
         """If increasing, use this unless your initial number and your stitch difference both are even and (in_num / st_dif) is uneven.
             If decreasing, use this unless (in_num - st_dif) and st_dif are both even and and ((in_num - st_dif)/ st_dif) is uneven"""
 
@@ -79,28 +85,79 @@ class Spacing:
         mid_reps_total = num_of_mid * sts_mid_rep # the total number of sts used in middle repeats
         remainder = self.in_num - mid_reps_total # the number of sts left for sts_start and sts_end when completing mid_reps
         sts_start = math.ceil(remainder / 2)  # the number of sts to be knitted before mid_reps
-        sts_end = math.ceil(remainder / 2)  # the number of sts to be knitted after mid_reps
+        sts_end = math.floor(remainder / 2)  # the number of sts to be knitted after mid_reps
         spacing_dict = {"sts_start": sts_start, "sts_mid_rep": sts_mid_rep, "num_of_mid": num_of_mid, "sts_end": sts_end}
-        # f'k{spacing_dict["sts_start"]}, m1, *k{spacing_dict["sts_mid_rep"]}, m1* {spacing_dict[num_of_mid]} times, k{spacing_dict["sts_end"]}')
+
         return spacing_dict
 
-    def even_divisor(self):
+    def odd_quotient(self):
         """If increasing, use this unless your initial number and your stitch difference both are even and (in_num / st_dif) is uneven.
             If decreasing, use this unless (in_num - st_dif) and st_dif are both even and and ((in_num - st_dif)/ st_dif) is uneven"""
-        num_mid = math.floor((self.in_num/self.st_dif) / 2) * 2 # the number of sts in middle of work
-        sts_side_rep = int((self.st_dif - 2) / 2) # the number of repeats between middle and either side
-        sts_in_rep = int(self.in_num/self.st_dif) # the number of sts in side repeats
-        sts_start = math.ceil((self.in_num/self.st_dif) / 2)
-        sts_end = math.ceil((self.in_num/self.st_dif) / 2)
-        spacing_dict = {"sts_start": sts_start, "sts_side_rep": sts_side_rep, "num_mid": num_mid, "sts_in_rep": sts_in_rep, "sts_end": sts_end}
+        # the number of sts in middle of work
+        num_mid = math.floor(self.st_quotient / 2) * 2
+        # the number of repeats between middle and either side
+        sts_side_rep = int((self.st_dif - 2) / 2)
+        # the number of sts in side repeats
+        sts_in_rep = int(self.st_quotient)
+        sts_start_end = math.ceil(self.st_quotient / 2)
+        spacing_dict = {"sts_start_end": sts_start_end, "sts_side_rep": sts_side_rep,
+                        "num_mid": num_mid, "sts_in_rep": sts_in_rep}
         return spacing_dict
 
-    #def get_spacing(self):
+    def horizontal_spacing(self):
+        if self.st_dif > 0:
+            in_num_mod = self.in_num % 2
+            st_dif_mod = self.st_dif % 2
+            if in_num_mod == st_dif_mod == 0 and self.st_quotient % 2 != 0:
+                spacing_dict = self.odd_quotient()
+                return f'k{spacing_dict["sts_start_end"]}, m1, *k{spacing_dict["sts_in_rep"]}, m1* ' \
+                       f'{spacing_dict["sts_side_rep"]} times, k{spacing_dict["num_mid"]}, m1, *k{spacing_dict["sts_in_rep"]}, m1* ' \
+                       f'k{spacing_dict["sts_side_rep"]} times, k{spacing_dict["sts_start_end"]}'
+            else:
+                spacing_dict = self.odd_even()
+                return f'k{spacing_dict["sts_start"]}, m1, *k{spacing_dict["sts_mid_rep"]}, m1* {spacing_dict["num_of_mid"]} times, ' \
+                       f'k{spacing_dict["sts_end"]}'
+        else: # if st_dif is negative, ie. when decreasing sts.
+            self.in_num += self.st_dif
+            self.st_dif *= -1
+            self.st_quotient = self.in_num / self.st_dif
+            in_num_mod = self.in_num % 2
+            st_dif_mod = self.st_dif % 2
+            if in_num_mod == st_dif_mod == 0 and self.st_quotient % 2 != 0:
+                spacing_dict = self.odd_quotient()
+                spacing_dict["sts_in_rep"] -= 1
+                spacing_dict["num_mid"] -= 2
+                return f'k{spacing_dict["sts_start_end"]}, dec 1, *k{spacing_dict["sts_in_rep"]}, dec 1* ' \
+                       f'{spacing_dict["sts_side_rep"]} times, k{spacing_dict["num_mid"]}, dec 1, *k{spacing_dict["sts_in_rep"]}, dec 1* ' \
+                       f'k{spacing_dict["sts_side_rep"]} times, k{spacing_dict["sts_start_end"]}'
+            else:
+                spacing_dict = self.odd_even()
+                spacing_dict["sts_start"] -= 1
+                spacing_dict["sts_end"] -=1
+                spacing_dict["sts_mid_rep"] -= 1
+                return f'k{spacing_dict["sts_start"]}, dec 1, *k{spacing_dict["sts_mid_rep"]}, dec 1* {spacing_dict["num_of_mid"]} times, ' \
+                       f'k{spacing_dict["sts_end"]}'
 
+    def vertical_spacing(self, num_of_rows):
+        if self.st_dif > 0:
+            remainder = num_of_rows % self.st_dif
+            num_of_inc_rows = int(self.st_dif / 2)
+            rows_per_rep = int((num_of_rows - remainder) / num_of_inc_rows)
+            return f'Work {rows_per_rep + 1} rows. Next row: work 1, increase 1, work to 1 st before end, increase 1, work 1. ' \
+                   f'Repeat inc row every {rows_per_rep} rows {num_of_inc_rows - 1} times ({num_of_inc_rows} times ' \
+                   f'total). Work {remainder} rows.'
+        else:
+            self.st_dif *= -1
+            remainder = num_of_rows % self.st_dif
+            num_of_dec_rows = int(self.st_dif / 2)
+            rows_per_rep = int((num_of_rows - remainder) / num_of_dec_rows)
+            return f'Work {rows_per_rep + 1} rows. Next row: work 1, decrease 1, work to 1 st before end, decrease, work 1. ' \
+                   f'Repeat dec row every {rows_per_rep} rows {num_of_dec_rows - 1} times ({num_of_dec_rows} times ' \
+                   f'total). Work {remainder} rows.'
 
-test = Spacing(20,4)
-spacing_dict = test.even_divisor()
-#print(spacing_dict["num_of_mid"])
-print(f'k{spacing_dict["sts_start"]}, m1, *k{spacing_dict["sts_in_rep"]}, m1* {spacing_dict["sts_side_rep"]} times, '
-      f'k{spacing_dict["num_mid"]}, m1, *k{spacing_dict["sts_in_rep"]}, m1* {spacing_dict["sts_side_rep"]} times, k{spacing_dict["sts_end"]}')
-#print(f'k{spacing_dict["sts_start"]}, m1, *k{spacing_dict["sts_mid_rep"]}, m1* {spacing_dict["num_of_mid"]} times, k{spacing_dict["sts_end"]}')
+test = Spacing(24, -14)
+print(test.vertical_spacing(30))
+spacing_dict = test.odd_even()
+
+print(f'k{spacing_dict["sts_start"]}, m1, *k{spacing_dict["sts_mid_rep"]}, m1* {spacing_dict["num_of_mid"]} times, ' \
+                       f'k{spacing_dict["sts_end"]}')
